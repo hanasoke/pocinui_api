7e26bae16e25a6db9a555bd2b8f6b88174e89ec6
hanasbp <hanasoke@gmail.com>
2026-09-17 18:41:25 +0700

    update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl = 'http://192.168.1.4:8000/api';
 
   static Future<Map<String, dynamic>> kirimKontak({
     required String nama,
